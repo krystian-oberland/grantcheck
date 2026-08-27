c77a740dc50292cc35e420a6e969a14cbb267a59
Krystian Oberland <krystianoberland@gmail.com>
2026-08-27 21:12:40 +0200

    Initial Streamlit project

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("GrantCheck Mini")
+st.write("Wstępna ocena dopasowania konkursów grantowych do profilu fundacji.")
+st.info("To jest wersja demonstracyjna aplikacji.")
